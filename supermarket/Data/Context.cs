8ff6ea1ca742fd971e8f6893c3cab556def86f5e
gustavoramosmarcondes <gustavo.ramos.marcondes@gmail.com>
2025-02-06 22:56:07 -0300

    primeiro commit

---
+++ supermarket/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using supermarket.Models;
+
+namespace supermarket.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<ProductsModel> products { get; set; }
+    }
+}
---
+++ supermarket/Models/ProductsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace supermarket.Models
+{
+    public class ProductsModel
+    {
+        public int id {  get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string category { get; set; }
+
+        public DateTime creationdate { get; set; }
+    }
+}
